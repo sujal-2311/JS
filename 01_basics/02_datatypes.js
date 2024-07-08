c3efec483b5392d75e829ecde9639306f50840f6
Sujal <sujalamirani@gmail.com>
2024-07-08 14:56:32 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using node.js, not browser
+
+console.log(3
+    +
+    3
+) //code readability should be high
+
+console.log("SUjal")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => when value is not assigned to a variable
+//symbol => unique
+
+
+//object
+
+console.log(typeof "sujal") // string
+console.log(typeof age) // number
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
