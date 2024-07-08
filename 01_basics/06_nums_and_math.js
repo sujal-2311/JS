3d601d436ec2da673e45d2ee46f6bbce5f552f9e
Sujal <sujalamirani@gmail.com>
2024-07-08 19:21:42 +0530

    nums and math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -3,3 +3,13 @@ console.log(score)
 
 const balance = new Number(100)
 console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toFixed(2))
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
